5eac42596773281357af742cb4d15f00270a30b4
ivanbadia <ivanbadia@gmail.com>
2021-03-20 16:05:03 +0100

    improve test readability

--- src/test/groovy/org/openchat/acceptance/WallFeature.groovy
+++ src/test/groovy/org/openchat/acceptance/WallFeature.groovy
@@ -9,21 +9,16 @@ class WallFeature extends ScenarioSpec<GivenUsers, WhenWall, ThenWall> {
 
     def "should show user's wall containing user's posts and followees' posts"() {
         expect:
-        given().users("rebeca", "daniela", "pep", "laura")
+        given().users("rebeca", "daniela", "pep")
                 .and()
                 .$_follows_$("rebeca", "daniela")
-                .and()
-                .$_follows_$("rebeca", "laura")
                 .and()
                 .$_creates_a_new_post_with_text_$("rebeca", "Hi!. I'm Rebeca")
                 .and()
                 .$_creates_a_new_post_with_text_$("daniela", "Hi!. I'm Daniela")
                 .and()
                 .$_creates_a_new_post_with_text_$("pep", "Hi!. I'm Pep")
-                .and()
-                .$_creates_a_new_post_with_text_$("laura", "Hi!. I'm Laura")
-                .and()
         when().$_checks_the_wall("rebeca")
-        then().the_posts_shown_are("Hi!. I'm Laura", "Hi!. I'm Daniela", "Hi!. I'm Rebeca")
+        then().the_posts_shown_are("Hi!. I'm Daniela", "Hi!. I'm Rebeca")
     }
 }
